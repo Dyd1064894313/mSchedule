04a623c6e45213cdba6d78c395d2b71d7dbd365b
duanyandong <1064894313@qq.com>
2018-08-03 17:18:02 +0800

    初始化

---
+++ mSchedule-core/src/main/java/top/duanyd/mschedule/task/TaskInfo.java
@@ -0,0 +1,23 @@
+package top.duanyd.mschedule.task;
+
+/**
+ * @Author: duanyandong
+ * @Date: 2018/8/3 10:45
+ * @Description:
+ */
+public class TaskInfo {
+
+    private String UUID;
+    private String name;
+    private String taskKind;
+    private int handleClassKind;
+    private String handleClassName;
+    private int allThreadNumber;
+    private String targetHostIPList;
+    private String workTimeIntervals;
+    private int fetchNumber;
+    private String startTime;
+    private String endTime;
+
+
+}
